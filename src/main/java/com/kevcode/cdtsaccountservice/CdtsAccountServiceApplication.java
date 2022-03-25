8d49144a7ff9c8ae2766784964e8a83a5dda1011
Kevin Pontón <kevin.ponton@devzeros.com>
2022-03-25 17:39:32 -0500

    first commit

---
+++ src/main/java/com/kevcode/cdtsaccountservice/CdtsAccountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kevcode.cdtsaccountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdtsAccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdtsAccountServiceApplication.class, args);
+	}
+
+}
